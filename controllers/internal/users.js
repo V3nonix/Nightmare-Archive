008c8369d7d99ae66179424bc0c2f2d82e966026
V3nonix <liam.grubbs@outlook.com>
2023-05-04 19:26:41 -0700

    Creates user controller.

---
+++ controllers/internal/users.js
@@ -0,0 +1,56 @@
+const User = require('../../models/user/user');
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const errorHandler = require('../../utils/errorHandler');
+
+
+function createJWT(user) {
+    return jwt.sign(
+        { user },
+        process.env.SECRET,
+        { expiresIn: '24h' }
+    );
+}
+
+
+async function create(req, res) {
+    try {
+        const user = await User.create(req.body);
+        await UserData.create({
+            user: user._id,
+            chatrooms: [],
+            groups: [],
+        });
+        const token = createJWT(user);
+        res.json(token);
+    } catch (err) {
+        errorHandler(__dirname, __filename, 'create', err, 500, res);
+    }
+}
+
+
+async function passwordCompare(password, hash) {
+    match = await bcrypt.compare(password, hash);
+    return match;
+}
+
+async function login(req, res) {
+    try {
+        const user = await User.findOne({ email: req.body.email});
+        if (user && passwordCompare(req.body.password, user.password)) {
+            const token = createJWT(user);
+            res.json(token);
+        } else {
+            res.status(400).json('Bad Credentials!');
+        }
+    } catch (err) {
+        errorHandler(__dirname, __filename, 'login', err, 500, res);
+    }
+}
+
+
+module.exports = {
+    create,
+    login,
+};
