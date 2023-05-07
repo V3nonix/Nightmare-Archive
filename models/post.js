a4c37c65aa93552d4a7e412a32707e24e48ccefc
V3nonix <liam.grubbs@outlook.com>
2023-05-06 17:00:09 -0700

    Completes Post model.

--- models/post.js
+++ models/post.js
@@ -2,6 +2,37 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Post Schema:
-// const postSchema = new Schema({
+const postSchema = new Schema({
+    title: {   
+        type: String, 
+        required: true,
+        maxlength: 32,
+        minlength: 1,
+    },
+    imageKey: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    content: {
+        type: String,
+        maxlength: 3000,
+        minlength: 1,
+        default: ''
+    },
+    comments: {
+        type: [mongoose.Schema.Types.ObjectId],
+        maxlength: 500,
+        ref: "Comment"
+    }
+});
 
-// });
+/* User Schema VIRTUALS */
+
+/* User Schema STATICS */
+
+/* User Schema MIDDLEWARE */
+
+module.exports = mongoose.model('Post', postSchema);
