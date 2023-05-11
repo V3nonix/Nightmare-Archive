fa9cfd9f5d919a480b6ec691d0b6468a7b2b085b
V3nonix <liam.grubbs@outlook.com>
2023-05-11 16:02:42 -0700

    Completes getPost api call function.

--- src/utilities/api/posts.js
+++ src/utilities/api/posts.js
@@ -5,17 +5,17 @@ import axios from 'axios';
 
 export async function sendPostData(reqFormData) {
     const headers= {
-      'Content-Type': 'multipart/form-data',
+        'Content-Type': 'multipart/form-data',
     }
     const token = getToken();
     if (token) {
-      headers.Authorization = `Bearer ${token}`;
+        headers.Authorization = `Bearer ${token}`;
     }
     try {
-      const response = await axios.post(`${BASE_URL}/create`, reqFormData, {headers});
-      return response.data;
+        const response = await axios.post(`${BASE_URL}/create`, reqFormData, {headers});
+        return response.data;
     } catch (err) {
-      throw err;
+        throw err;
     }
 }
 
@@ -29,4 +29,8 @@ export async function updatePost(postId, update) {
 
 export async function getUserPosts() {
     return sendRequest(`${BASE_URL}/user`);
+}
+
+export async function getPost(postId, publicStatus) {
+    return sendRequest(`${BASE_URL}/${postId}?public=${publicStatus}`);
 }
